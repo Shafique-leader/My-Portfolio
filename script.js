641e3d608e9d5b4c67ebb6a4f83c8833041ebfc0
Shafique <leaders2025pk@gmail.com>
2026-09-24 09:22:39 +0500

    add js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Thank you! Your message has been received.");
+
+});
